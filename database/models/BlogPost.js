cfff1ac071909285b1423f19662133ad7967ddaa
KhanhHoangNam <chienbinhbattu9xpro@gmail.com>
2019-11-02 23:13:01 +0700

    query blog posts by range date

--- database/models/BlogPost.js
+++ database/models/BlogPost.js
@@ -34,7 +34,46 @@ const insertBlogPost = async(title, content, tokenKey) => {
     }
 }
 //Muốn xem danh sách các blogpost => không cần token
+//Vì ai cũng thế xem được
+const queryBlogPosts = async (text) => {
+    try {        
+        let blogPosts = await BlogPost.find({
+            $or: [
+                {
+                    title: new RegExp(text, "i")
+                    //i => ko phân biệt hoa/thường
+                },
+                {
+                    content: new RegExp(text, "i")
+                }
+            ],                   
+        })
+        return blogPosts
+    } catch(error) {        
+        throw error
+    }
+}
+//Lấy các bài viết post trong khoảng ngày A => ngày B
+const queryBlogPostsByDateRange = async (from, to) => {
+    //format: dd--mm--yyyy
+    let fromDate = new Date(parseInt(from.split('-')[2]),
+                            parseInt(from.split('-')[1])-1,
+                            parseInt(from.split('-')[0]))
+    let toDate = new Date(parseInt(to.split('-')[2]),
+                          parseInt(to.split('-')[1])-1,
+                          parseInt(to.split('-')[0]))      
+    try {
+        let blogPosts = await BlogPost.find({
+            date: {$gte: fromDate, $lte: toDate}
+        })
+        return blogPosts
+    } catch (error) {
+        throw error
+    }                                             
+}
 module.exports = {
     BlogPost,
-    insertBlogPost
+    insertBlogPost,
+    queryBlogPosts,
+    queryBlogPostsByDateRange
 }
--- routers/blogPostRouter.js
+++ routers/blogPostRouter.js
@@ -5,7 +5,9 @@
 const express = require('express')
 const router = express.Router()
 const {
-    insertBlogPost
+    insertBlogPost,
+    queryBlogPosts,
+    queryBlogPostsByDateRange
 } = require('../database/models/BlogPost')
 router.use((req, res, next) => {
     console.log('Time: ', Date.now()) //Timelog
@@ -26,6 +28,39 @@ router.post('/insertBlogPost', async (req, res) => {
         res.json({
             result: 'failed',
             message: `Không thể thêm mới BlogPost. Lỗi: ${error}`
+        })
+    }
+})
+router.get('/queryBlogPosts', async(req, res) => {
+    let {text} = req.query
+    try {
+        let blogPosts = await queryBlogPosts(text)
+        res.json({
+            result: "success",
+            message: "Query thành công danh sách BlogPost",
+            data: blogPosts
+        })
+    } catch (error) {
+        res.json({
+            result: "failed",
+            message: `Không thể lấy được danh sách blogPosts. Lỗi: ${error}`,            
+        })
+    }
+})
+//
+router.get('/queryBlogPostsByDateRange', async(req, res) => {
+    let {from, to} = req.query
+    try {
+        let blogPosts = await queryBlogPostsByDateRange(from, to)
+        res.json({
+            result: "success",
+            message: "Query thành công danh sách BlogPost",
+            data: blogPosts
+        })
+    } catch (error) {
+        res.json({
+            result: "failed",
+            message: `Không thể lấy được danh sách blogPosts. Lỗi: ${error}`,            
         })
     }
 })
