0a5b2f9b718eafe21607752c7ce59b6447c565e0
KhanhHoangNam <chienbinhbattu9xpro@gmail.com>
2019-11-03 02:39:05 +0700

    delete blopost

--- database/models/BlogPost.js
+++ database/models/BlogPost.js
@@ -71,9 +71,78 @@ const queryBlogPostsByDateRange = async (from, to) => {
         throw error
     }                                             
 }
+//Lấy nội dung chi tiết 1 BlogPost => Không cần token
+const getDetailBlogPost = async (blogPostId) => {
+    let blogPost = await BlogPost.findById(blogPostId)
+    try {
+        if(!blogPost) {
+            throw `Không tìm thấy blogpost với Id=${blogPostId}`
+        }
+        return blogPost
+    } catch (error) {
+        throw error
+    }
+}
+//Cập nhật 1 blogpost => yêu cầu token
+//Chỉ có tác giả mới cập nhật được BlogPost của mình
+const updateBlogPost = async(blogPostId, updatedBlogPost, tokenKey) => {
+    try {
+        let signedInUser = await verifyJWT(tokenKey)
+        let blogPost = await BlogPost.findById(blogPostId)
+        try {
+            if(!blogPost) {
+                throw `Không tìm thấy blogpost với Id=${blogPostId}`
+            }
+            if(signedInUser.id !== blogPost.author.toString()) {
+                throw "Không update được vì bạn không phải là tác giả bài viết"
+            }
+            blogPost.title = !updatedBlogPost.title ? blogPost.title : updatedBlogPost.title
+            blogPost.content = !updatedBlogPost.content ? blogPost.content : updatedBlogPost.content
+            blogPost.date = Date.now()
+            await blogPost.save()
+            return blogPost
+        } catch (error) {
+            throw error
+        }
+    } catch (error) {
+        throw error
+    }
+}
+//Xóa 1 bản ghi blogPost:
+//1. Xóa bản ghi trong bảng BlogPosts
+//2. Cập nhật trường tham chiếu "blogPosts" trong bảng Users
+//=> Mảng bớt đi một phần tử
+const deleteBlogPost = async(blogPostId, tokenKey) =>{
+    try {
+        let signedInUser = await verifyJWT(tokenKey)
+        let blogPost = await BlogPost.findById(blogPostId)
+        try {
+            if(!blogPost) {
+                throw `Không tìm thấy blogpost với Id=${blogPostId}`
+            }
+            if(signedInUser.id !== blogPost.author.toString()) {
+                throw "Không xóa được vì bạn không phải là tác giả bài viết"
+            }
+            await BlogPost.deleteOne({_id: blogPostId})
+            signedInUser.blogPosts = await signedInUser.blogPosts.filter(
+                eachBlogPost => {
+                    return blogPost._id.toString() !== eachBlogPost._id.toString()
+                }
+            )
+            await signedInUser.save()
+        } catch (error) {
+            throw error
+        }
+    } catch (error) {
+        throw error
+    }
+}
 module.exports = {
     BlogPost,
     insertBlogPost,
     queryBlogPosts,
-    queryBlogPostsByDateRange
+    queryBlogPostsByDateRange,
+    getDetailBlogPost,
+    updateBlogPost,
+    deleteBlogPost
 }
--- routers/blogPostRouter.js
+++ routers/blogPostRouter.js
@@ -7,7 +7,10 @@ const router = express.Router()
 const {
     insertBlogPost,
     queryBlogPosts,
-    queryBlogPostsByDateRange
+    queryBlogPostsByDateRange,
+    getDetailBlogPost,
+    updateBlogPost,
+    deleteBlogPost
 } = require('../database/models/BlogPost')
 router.use((req, res, next) => {
     console.log('Time: ', Date.now()) //Timelog
@@ -38,7 +41,7 @@ router.get('/queryBlogPosts', async(req, res) => {
         res.json({
             result: "success",
             message: "Query thành công danh sách BlogPost",
-            data: blogPosts
+            data: blogPosts,            
         })
     } catch (error) {
         res.json({
@@ -55,12 +58,63 @@ router.get('/queryBlogPostsByDateRange', async(req, res) => {
         res.json({
             result: "success",
             message: "Query thành công danh sách BlogPost",
-            data: blogPosts
+            data: blogPosts            
         })
     } catch (error) {
         res.json({
             result: "failed",
             message: `Không thể lấy được danh sách blogPosts. Lỗi: ${error}`,            
+        })
+    }
+})
+router.get('/getDetailBlogPost', async(req, res) => {
+    let {blogPostId} = req.query
+    try {
+        let blogPost = await getDetailBlogPost(blogPostId)
+        res.json({
+            result: "success",
+            message: "Get detail blogpost successfully!",
+            data: blogPost
+        })
+    } catch (error) {
+        res.json({
+            result: "failed",
+            message: `Cannot get deatail blogpost. Error: ${error}`
+        })
+    }
+})
+//
+router.put('/updateBlogPost', async(req, res) => {
+    let {blogPostId} = req.body
+    let updatedBlogPost = req.body
+    let tokenKey = req.headers['x-access-token']
+    try {
+        let blogPost = await updateBlogPost(blogPostId, updatedBlogPost, tokenKey)
+        res.json({
+            result: "success",
+            message: "Update blogpost successfully!",
+            data: blogPost
+        })
+    } catch (error) {
+        res.json({
+            result: "failed",
+            message: `Cannot update blogpost. Error: ${error}`
+        })
+    }
+})
+router.delete('/deleteBlogPost', async(req, res) => {
+    let {blogPostId} = req.body    
+    let tokenKey = req.headers['x-access-token']
+    try {
+        await deleteBlogPost(blogPostId, tokenKey)
+        res.json({
+            result: "success",
+            message: "Delete blogpost sucessfully!"
+        })
+    } catch (error) {
+        res.json({
+            result: "failed",
+            message: `Cannot delete blogpost. Error: ${error}`
         })
     }
 })
